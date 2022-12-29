ba283670863d063c811ff608066c4a4f9928aab0
Hector Santos <jcsnuesi@gmail.com>
2022-12-29 12:09:30 -0400

    ignore created

--- index.js
+++ index.js
@@ -6,11 +6,20 @@ var port = process.env.PORT || 3990
 
 mongoose.Promise = global.Promise
 
-mongoose.connect('mongodb://127.0.0.1:27017/api-inv', {useNewUrlParser: true})
+//Importar variable de entorno locales
+require('dotenv').config({ path:'variables.env'})
+
+//Leer localhost de variables y puerto
+const host = process.env.HOST || '0.0.0.0'
+
+
+console.log(process.env.DB_URL)
+
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true})
         .then(() => {
             console.log('Conectado a la BD!')
 
-            app.listen(port, ()=>{
+            app.listen(port, host, ()=>{
                 console.log('Servidor corriendo.')
             })
         }).catch(error => console.log(error))
